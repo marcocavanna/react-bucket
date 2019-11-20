f1e49a1f62996f589785652c1ec6aba2992a47e5
Marco Cavanna <info@marcocavanna.com>
2019-11-20 11:23:05 +0100

    Gulp Removed

--- gulp/plugins/util/index.js
+++
@@ -1,5 +0,0 @@
-export getComponentInfo from './getComponentInfo';
-export parseDefaultValue from './parseDefaultValue';
-export parseDocblock from './parseDocblock';
-export parserCustomHandler from './parserCustomHandler';
-export parseType from './parseType';
