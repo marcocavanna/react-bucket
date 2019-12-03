ec19d1b3fc65a76ef1f58070696e959113be301b
Marco Cavanna <info@marcocavanna.com>
2019-12-03 17:26:18 +0100

    Extend the Compute Value for Formik Select

--- src/react/collections/Form/FormikSelect.js
+++ src/react/collections/Form/FormikSelect.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { isObject } from '@appbuckets/rabbit';
+
 import _ from 'lodash';
 
 import getFormFieldStateProps from './lib/get-form-field-state-props';
 import withFormikField from './lib/with-formik-field';
 
 import Select from '../../elements/Select';
+
+const computeElementValue = value => (
+  isObject(value)
+    ? value._id || value.value
+    : value
+);
 
 const FormikSelectComponent = ({ state, meta, rest }) => (
   <Select
@@ -32,11 +40,25 @@ const FormikSelect = withFormikField({
     /** On Multi selector must append value on original array */
     formik.setFieldValue(props.name, selected);
   },
-  computeValue: (value, { options }) => (
-    value && Array.isArray(options)
-      ? options.find(opt => opt.value === value)
-      : null
-  )
+  computeValue: (value, { options, ...props }) => {
+    /** On Simple Selector, try to compute the value */
+    if (!props.isMulti) {
+      return (
+        value && Array.isArray(options)
+          ? options.find(opt => opt.value === value)
+          : null
+      );
+    }
+
+    const selectedValues = Array.isArray(value) ? value.map(computeElementValue) : [];
+
+    /** On Multiselector must build an array of value? */
+    const selected = Array.isArray(value) && Array.isArray(options)
+      ? options.filter(option => selectedValues.includes(option.value))
+      : [];
+
+    return selected;
+  }
 });
 
 export default FormikSelect;
