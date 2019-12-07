641c70b32afdf17a11161cf1956b68f5fd540349
Matteo Ballarini <matteo@appbuckets.io>
2019-12-07 15:08:09 +0100

    Label Docs

--- docs/app/docs/elements/Label/index.js
+++ docs/app/docs/elements/Label/index.js
@@ -24,6 +24,83 @@ export default {
 
   examples: {
 
+    standard: {
+      header    : 'Label',
+      subheader : 'A standard label',
+      content   : (
+        <React.Fragment>
+          <Label
+            content='Label'
+          />
+        </React.Fragment>
+      )
+    },
+
+    icon: {
+      header    : 'Icon',
+      subheader : 'A Label can be formatted with an icon',
+      content   : (
+        <React.Fragment>
+          <Label
+            icon='info'
+            content='information'
+          />
+        </React.Fragment>
+      )
+    },
+
+    colors: {
+      header    : 'Colored',
+      subheader : 'A Label can be formatted with different colors',
+      content   : (
+        <React.Fragment>
+          <Label
+            content='primary'
+            color='primary'
+          />
+          <Label
+            content='secondary'
+            color='secondary'
+          />
+          <Label
+            content='danger'
+            color='danger'
+          />
+          <Label
+            content='success'
+            color='success'
+          />
+        </React.Fragment>
+      )
+    },
+
+    deatil: {
+      header    : 'Detail',
+      subheader : 'A Label can be formatted with some details',
+      content   : (
+        <React.Fragment>
+          <Label
+            content='Label'
+            detail='Label detail'
+          />
+        </React.Fragment>
+      )
+    },
+
+    group: {
+      header    : 'Group',
+      subheader : 'It is possible to create Groups of Label',
+      content   : (
+        <React.Fragment>
+          <Label.Group>
+            <Label content='first' />
+            <Label content='second' />
+            <Label content='third' />
+          </Label.Group>
+        </React.Fragment>
+      )
+    }
+
   }
 
 };
