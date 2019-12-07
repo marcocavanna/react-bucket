a1008096385507110d83dbc782d9ddd2c7793058
Matteo Ballarini <matteo@appbuckets.io>
2019-12-07 15:08:40 +0100

    Toast Docs

--- docs/app/docs/elements/Toast/index.js
+++ docs/app/docs/elements/Toast/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import ButterToast from 'butter-toast';
+
 import loadComponentInfo from '../../util/loadComponentInfo';
 import ToastInfo from '../../component-info/Toast.info.json';
+import { Toast } from './NotificationManager';
 
-import { Toast } from '../../../../../src/react';
+import { Button } from '../../../../../src/react';
 
 export default {
   path: '/toast',
@@ -23,6 +26,106 @@ export default {
   },
 
   examples: {
+
+    standard: {
+      header    : 'Toast',
+      subheader : 'Simple Toast notification',
+      content   : () => {
+
+        const handleClick = () => {
+
+          Toast.info('Clicked');
+
+        };
+
+        return (
+          <React.Fragment>
+            <Button content='click' onClick={handleClick} />
+
+            <ButterToast
+              className='toasts'
+              namespace='toast-container'
+              position={{
+                horizontal : 'POS_CENTER',
+                vertical   : 'POS_BOTTOM'
+              }}
+            />
+          </React.Fragment>
+        );
+      }
+    },
+
+    error: {
+      header    : 'Error',
+      subheader : 'A Toast can notificate an error',
+      content   : () => {
+
+        const handleClick = () => {
+          Toast.error('Error');
+        };
+
+        return (
+          <React.Fragment>
+            <Button danger content='Error Toast' onClick={handleClick} />
+          </React.Fragment>
+        );
+
+      }
+    },
+
+    primary: {
+      header    : 'Primary',
+      subheader : 'A Toast can be formatted in primary style',
+      content   : () => {
+
+        const handleClick = () => {
+          Toast.primary('Clicked');
+        };
+
+        return (
+          <React.Fragment>
+            <Button primary content='primary toast' onClick={handleClick} />
+          </React.Fragment>
+        );
+
+      }
+    },
+
+    success: {
+      header    : 'Success',
+      subheader : 'A Toast can be success',
+      content   : () => {
+
+        const handleClick = () => {
+          Toast.success('Action Complete');
+        };
+
+        return (
+          <React.Fragment>
+            <Button success content='Success Toast' onClick={handleClick} />
+          </React.Fragment>
+        );
+
+      }
+    },
+
+    warning: {
+      header    : 'Warning',
+      subheader : 'A Toast can be warning',
+      content   : () => {
+
+        const handleClick = () => {
+          Toast.warning('Warning!');
+        };
+
+        return (
+          <React.Fragment>
+            <Button warning content='Warning Toast' onClick={handleClick} />
+          </React.Fragment>
+        );
+
+      }
+    }
 
   }
 
