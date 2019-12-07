097f8c1b800498131cc13cabf8fcd6f383adebef
Matteo Ballarini <matteo@appbuckets.io>
2019-12-07 15:08:28 +0100

    Spacer Docs

--- docs/app/docs/elements/Spacer/index.js
+++ docs/app/docs/elements/Spacer/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 import loadComponentInfo from '../../util/loadComponentInfo';
 import SpacerInfo from '../../component-info/Spacer.info.json';
 
-import { Spacer } from '../../../../../src/react';
+import { Spacer, Image, Divider, Header } from '../../../../../src/react';
+
+import Img from '../Image/short-paragraph.png';
 
 export default {
   path: '/spacer',
@@ -23,6 +25,44 @@ export default {
   },
 
   examples: {
+
+    standard: {
+      header    : 'Spacer',
+      subheader : 'A space divide two content',
+      content   : (
+        <React.Fragment>
+          <Image src={Img} />
+          <Spacer height='7' />
+          <Image src={Img} />
+        </React.Fragment>
+      )
+    },
+
+    height: {
+      header    : 'Height',
+      subheader : 'A spacer can be formatted with 8 different Height, from 1 to 8',
+      content   : (
+        <React.Fragment>
+          <Header content='Section one' subheader='height 1' />
+          <Image src={Img} />
+          <Spacer height='1' />
+          <Header content='Section two' />
+          <Image src={Img} />
+          <Divider />
+          <Header content='Section one' subheader='height 5' />
+          <Image src={Img} />
+          <Spacer height='5' />
+          <Header content='Section two' />
+          <Image src={Img} />
+          <Divider />
+          <Header content='Section one' subheader='heigt 8' />
+          <Image src={Img} />
+          <Spacer height='8' />
+          <Header content='Section two' />
+          <Image src={Img} />
+        </React.Fragment>
+      )
+    }
 
   }
 
