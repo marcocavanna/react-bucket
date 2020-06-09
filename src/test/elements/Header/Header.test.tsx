1ef524a3d59be0d1419823d3c4244556cdcf4a5f
Marco Cavanna <marco@appbuckets.io>
2020-06-09 17:10:49 +0200

    test(header): created header element test

---
+++ src/test/elements/Header/Header.test.tsx
@@ -0,0 +1,38 @@
+import { Header } from '../../../react/elements/Header';
+
+import * as common from '../../common';
+
+
+describe('testing header', () => {
+
+  common.assertComponentDeclaration(Header, { filePath: [ 'elements', 'Header' ] });
+
+  common.componentCorrectlyRender(Header, { hasClassName: [ 'header' ], injectChildren: false });
+
+  common.implementsCreateMethod(Header);
+
+  common.itWillHaveSharedClassName(Header);
+
+});
+
+
+describe('testing header content', () => {
+
+  common.assertComponentDeclaration(Header.Content, { filePath: [ 'elements', 'Header' ] });
+
+  common.componentCorrectlyRender(Header.Content, { hasClassName: [ 'content' ] });
+
+  common.implementsCreateMethod(Header.Content);
+
+});
+
+
+describe('testing header subheader', () => {
+
+  common.assertComponentDeclaration(Header.Subheader, { filePath: [ 'elements', 'Header' ] });
+
+  common.componentCorrectlyRender(Header.Subheader, { hasClassName: [ 'subheader' ] });
+
+  common.implementsCreateMethod(Header.Subheader);
+
+});
