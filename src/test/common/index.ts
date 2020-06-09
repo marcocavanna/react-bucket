0f631bac8a172235ed595f309154645b5dd4f181
Marco Cavanna <marco@appbuckets.io>
2020-06-09 17:08:08 +0200

    test(common): added common testing functions

--- src/test/common/index.ts
+++ src/test/common/index.ts
@@ -3,3 +3,7 @@ export { default as componentCorrectlyRender } from './componentCorrectlyRender'
 export { default as assertComponentDeclaration } from './componentDeclaration';
 
 export { default as implementsCreateMethod } from './implementsCreateMethod';
+
+export { default as itWillHaveSharedClassName } from './itWillHaveSharedClassName';
+
+export * from './itWillSetResponsiveClassName';
