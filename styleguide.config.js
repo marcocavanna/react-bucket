8090878ada7829d57c2919e5ffa063e1b8873ef1
Marco Cavanna <marco@appbuckets.io>
2020-06-11 15:28:30 +0200

    docs(styleguidist): added styleguidis package, script and configuration

---
+++ styleguide.config.js
@@ -0,0 +1,37 @@
+const reactDocgenTypescript = require('react-docgen-typescript');
+
+module.exports = {
+  /** Set Typescript as PropsParser */
+  propsParser  : reactDocgenTypescript.withDefaultConfig({
+    /** Remove Props of React Core Component */
+    propFilter                        : (prop) => {
+      if (/^(DOMAttributes|AriaAttributes|HTMLAttributes|Attributes)$/.test(prop.parent.name)) {
+        return false;
+      }
+
+      console.log(prop);
+
+      return true;
+    },
+    /** Extract Literal Value from Type */
+    shouldExtractLiteralValuesFromEnum: true
+  }).parse,
+  /** Set Components Folder */
+  components   : './src/react/**/*.tsx',
+  /** Ignore some files */
+  ignore       : [
+    './src/react/**/*.stories.tsx',
+    './src/react/stories.tsx'
+  ],
+  /** Set a minimal webpack config */
+  webpackConfig: {
+    module: {
+      rules: [
+        {
+          test  : /\.tsx?$/,
+          loader: 'ts-loader'
+        }
+      ]
+    }
+  }
+};
