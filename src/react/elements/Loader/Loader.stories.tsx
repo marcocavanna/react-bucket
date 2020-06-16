c4f1cf34607ce01b3862f106090bb84d4236997e
Marco Cavanna <marco@appbuckets.io>
2020-06-16 19:46:32 +0200

    fix(loader): fixed some loader styles

--- src/react/elements/Loader/Loader.stories.tsx
+++ src/react/elements/Loader/Loader.stories.tsx
@@ -7,5 +7,5 @@ export default { title: 'Elements/Loader', component: Loader };
 
 
 export const defaultLoader = () => (
-  <Loader centered primary content={'Caricamento'} size={'big'} type={'circular dots'} />
+  <Loader centered primary content={'Caricamento'} type={'indeterminate bar'} />
 );
