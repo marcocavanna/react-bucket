f4d414039fd181ae9fd403e9bbfaa5547ef56a64
Marco Cavanna <marco@appbuckets.io>
2020-10-07 12:09:51 +0200

    chore(plop): updated template files

--- plopfile.ts
+++ plopfile.ts
@@ -3,35 +3,93 @@ import { NodePlopAPI } from 'plop';
 
 export default function buildGenerators(plop: NodePlopAPI) {
 
+  /** Set the Component Type */
+  const componentTypePrompts = {
+    type   : 'list',
+    name   : 'type',
+    message: 'Component Type',
+    choices: [ 'collections', 'elements', 'modules', 'widget' ]
+  };
+
+  /** Set the Component Extras */
+  const componentExtras = [
+    {
+      type   : 'confirm',
+      name   : 'couldHaveChildren',
+      message: 'This component could have declared children?'
+    },
+    {
+      type   : 'confirm',
+      name   : 'couldChangeElementType',
+      message: 'This component could change its default ElementType?'
+    },
+    {
+      type   : 'confirm',
+      name   : 'hasStateClasses',
+      message: 'This components could has component state classes, like primary/danger etc?'
+    }
+  ];
+
+
+  /* --------
+   * Build Generators
+   * -------- */
   plop.setGenerator('component', {
     description: 'React Bucket Component',
     prompts    : [
-      {
-        type   : 'input',
-        name   : 'type',
-        message: 'Component Type'
-      },
+      componentTypePrompts,
       {
         type   : 'input',
         name   : 'componentName',
         message: 'The Component Name'
-      }
+      },
+      ...componentExtras
     ],
     actions    : [
       {
         type        : 'add',
-        path        : 'src/react/{{ type }}/{{ componentName }}/{{ pascalCase componentName }}.tsx',
+        path        : 'src/react/{{ type }}/{{ pascalCase componentName }}/{{ pascalCase componentName }}.tsx',
         templateFile: 'templates/component.hbs'
       },
       {
         type        : 'add',
-        path        : 'src/react/{{ type }}/{{ componentName }}/{{ pascalCase componentName }}.types.ts',
+        path        : 'src/react/{{ type }}/{{ pascalCase componentName }}/{{ pascalCase componentName }}.types.ts',
         templateFile: 'templates/component.types.hbs'
       },
       {
         type        : 'add',
-        path        : 'src/react/{{ type }}/{{ pascalCase componentName }}/index.ts',
+        path        : 'src/react/{{ type }}/{{ pascalCase pascalCase componentName }}/index.ts',
         templateFile: 'templates/component.index.hbs'
+      }
+    ]
+  });
+
+  plop.setGenerator('subcomponent', {
+    description: 'React Bucket Sub Component',
+    prompts    : [
+      componentTypePrompts,
+      {
+        type   : 'input',
+        name   : 'parentComponentName',
+        message: 'Parent Component Name'
+      },
+      {
+        type   : 'input',
+        name   : 'componentName',
+        message: 'Component Name'
+      },
+      ...componentExtras
+    ],
+    actions    : [
+      {
+        type        : 'add',
+        path        : 'src/react/{{ type }}/{{ pascalCase parentComponentName }}/{{ pascalCase componentName }}.tsx',
+        templateFile: 'templates/component.hbs'
+      },
+      {
+        type        : 'add',
+        path        : 'src/react/{{ type }}/{{ pascalCase parentComponentName }}/{{ pascalCase componentName }}.types.ts',
+        templateFile: 'templates/component.types.hbs'
       }
     ]
   });
